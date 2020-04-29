6519c23331420e650da84cc3c997f4e1a3f441c1
s3751881 <tr.baonhi3008@gmail.com>
2020-04-29 13:35:02 +0700

    modify navigation

--- a3/script.js
+++ a3/script.js
@@ -25,14 +25,16 @@ let movies_syn = {
     'synopsisACT': {
         name: 'Avengers: Endgame',
         rating: 'M',
-        time: [ 'Wednesday: 9pm', 'Thursday: 9pm', 'Friday 9pm', 'Saturday: 6pm ', 'Sunday: 6pm'],
+        movieday: [ 'Wednesday: 9pm', 'Thursday: 9pm', 'Friday 9pm', 'Saturday: 6pm ', 'Sunday: 6pm'],
+        hour:['-', '-', '21:00', '21:00', '21:00', '18:00', '18:00'],
         plot: String.raw`After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...`,
         trailer: 'https://www.youtube.com/embed/TcMBFSGVi1c'
     },
     'synopsisAHF': {
         name: 'The Happy Prince',
         rating: 'MA15+',
-        time: ['12:00', '12:00', '12:00', '21:00', '21:00'],
+        movieday:['Wednesday','Thursday','Saturday','Sunday'],
+        hour: ['-', '-', '12:00', '12:00', '12:00', '21:00', '21:00'],
         plot: String.raw`In a cheap Parisian hotel room Oscar Wilde lies on his death bed. The past floods back, taking him to other times and places. Was he once the most famous man in London? The artist crucified by a society that once worshipped him? Under the microscope of death he reviews the failed attempt to reconcile with his long suffering wife Constance, the ensuing reprisal of his fatal love affair with Lord Alfred Douglas and the warmth and devotion of Robbie Ross, who tried and failed to save him from himself. Travelling through Wilde's final act and journeys through England, France and Italy, the transience of lust is laid bare and the true riches of love are revealed. It is a portrait of the dark side of a genius who lived and died for love.`,
         trailer: 'https://www.youtube.com/embed/4HmN9r1Fcr8'
     },
@@ -40,14 +42,16 @@ let movies_syn = {
     'synopsisRMC': {
         name: 'Top End Wedding',
         rating: 'M',
-        time: ['18:00', '18:00', '15:00', '15:00'],
+        movieday:['Monday','Tuesday','Saturday','Sunday'],
+        hour: ['18:00', '18:00', '-', '-', '-', '15:00', '15:00'],
         plot: String.raw`Lauren and Ned are engaged, they are in love, and they have just ten days to find Lauren's mother who has gone AWOL somewhere in the remote far north of Australia, reunite her parents and pull off their dream wedding.`,
         trailer: 'https://www.youtube.com/embed/uoDBvGF9pPU'
     },
     'synopsisANM': {
         name: 'Dumbo',
         rating: 'PG',
-        time: ['12:00', '12:00', '18:00', '18:00', '18:00', '12:00', '12:00'],
+        movieday:['Monday','Tuesday','Wednesday','Thursday','Saturday','Sunday'],
+        hour: ['12:00', '12:00', '18:00', '18:00', '18:00', '12:00', '12:00'],
         plot: String.raw`Holt was once a circus star, but he went off to war and when he returned it had terribly altered him. Circus owner Max Medici (Danny DeVito) hires him to take care of Dumbo, a newborn elephant whose oversized ears make him the laughing stock of the struggling circus troupe. But when Holt's children discover that Dumbo can fly, silver-tongued entrepreneur V.A. Vandevere (Michael Keaton), and aerial artist Colette Marchant (Eva Green) swoop in to make the little elephant a star.`,
         trailer: 'https://www.youtube.com/embed/7NiYVoqBt-8'
     },
@@ -72,25 +76,70 @@ function showContent(event){
     trailer.src = movie.trailer;
     movie_syn_input.value = event.target.id;
     synopsis_sec.style.display = '';
-    createDetailContainer(movie.time);
+    movie_showtime = movie.movieday + movie.hour
+    createDetailContainer(movie_showtime);
     console.log('clicked')
 }
 
 
-const createTimeButton = (time, index) => {
+// const createTimeButton = (time, index) => {
+//     let button = document.createElement('Button');
+//     button.setAttribute('class', 'btn btn-secondary movie_hour');
+//     button.setAttribute('style', 'margin: 2% 2% 2% 0;');
+//     button.setAttribute('id', 'movie_hour_' + index);
+//     button.onclick = (e) => e.preventDefault();
+// //    create span
+//     let information= document.createElement('SPAN');
+//     information.innerText = time;
+//     information.setAttribute('class', 'day');
+//     button.appendChild(information)
+//     return button
+// }
+const createTimeButton = (hour,day,index) => {
     let button = document.createElement('Button');
     button.setAttribute('class', 'btn btn-secondary movie_hour');
     button.setAttribute('style', 'margin: 2% 2% 2% 0;');
     button.setAttribute('id', 'movie_hour_' + index);
     button.onclick = (e) => e.preventDefault();
-//    create span
-    let information = document.createElement('SPAN');
-    information.innerText = time;
-    information.setAttribute('class',  'day');
-    button.appendChild(information)
+//    create span day 
+    let information_day= document.createElement('SPAN');
+    information_day.innerText = day;
+    information_day.setAttribute('class', 'day');
+    let information_hour = document.createElement('SPAN');
+    information_hour.innerText = hour;
+    information_hour.setAttribute('class','hour')
+    button.appendChild(information_day,information_hour)
+// hour   
     return button
 }
 
+// const createTimeButton = (time, index) => {
+//     let button = document.createElement('Button');
+//     button.setAttribute('class', 'btn btn-secondary movie_hour');
+//     button.setAttribute('style', 'margin: 2% 2% 2% 0;');
+//     button.setAttribute('id', 'movie_hour_' + index);
+//     button.onclick = (e) => e.preventDefault();
+// //    create span
+//     let information = document.createElement('SPAN');
+//     information.innerText = time;
+//     information.setAttribute('class',  'day');
+//     button.appendChild(information)
+//     return button
+// }
+
+// const createDetailContainer = (time)=>{
+//     let index = 0;
+//     let detailContainer = document.getElementById('timetable');
+//     detailContainer.innerHTML = '';
+//     detailContainer.setAttribute('class', 'col-sm-9');
+//     detailContainer.setAttribute('style', 'color:black; display:block');
+//     detailContainer.setAttribute('id', 'timetable');
+//     for (let item of time) {
+//         const detailButton = createTimeButton(item, index);
+//         index++;
+//         detailContainer.append(detailButton);
+//     }
+// }
 const createDetailContainer = (time)=>{
     let index = 0;
     let detailContainer = document.getElementById('timetable');
@@ -98,13 +147,90 @@ const createDetailContainer = (time)=>{
     detailContainer.setAttribute('class', 'col-sm-9');
     detailContainer.setAttribute('style', 'color:black; display:block');
     detailContainer.setAttribute('id', 'timetable');
-    for (let item of time) {
-        const detailButton = createTimeButton(item, index);
+    for (let item1,item2 of time) {
+        const detailButton = createTimeButton(item1,item2,index);
         index++;
         detailContainer.append(detailButton);
     }
 }
 
+
 buttons.forEach(button => {button.addEventListener('click',(event)=>showContent(event))})
 
 
+function hour(obj) {
+    x = obj.children[0].innerHTML
+    y = obj.children[1].innerHTML
+    var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    if (x == "Monday" || x == "Wednesday" || (Number(y) == 12 && weekdays.includes(x) == true)) {
+        return document.getElementById('results').innerHTML = 'Sale';
+    }
+    else {
+        return document.getElementById('results').innerHTML = 'Non_sale';
+    }
+}
+
+function seat_id_value(seat_id) {
+    var seat_price = document.getElementById('seat[' + seat_id + ']')
+    return document.getElementById('value_results_' + seat_id).innerHTML = seat_price.value   
+}
+
+function total() {
+    var seat_price_array = {
+        STA: {
+            Sale: 14.00,
+            Non_sale: 19.80
+        },
+        STP: {
+            Sale: 12.50,
+            Non_sale: 17.50
+        },
+        STC: {
+            Sale: 11.00,
+            Non_sale: 15.30
+        },
+        FCA: {
+            Sale: 22.50,
+            Non_sale: 27.00
+        },
+        FCP: {
+            Sale: 24.00,
+            Non_sale: 30.00
+        },
+        FCC: {
+            Sale: 21.00,
+            Non_sale: 24.00
+        },
+
+    };
+    var sale_or_non_sale = document.getElementById('results').innerHTML
+    var seat_value_STA = document.getElementById('value_results_STA').innerHTML
+    var seat_value_STP = document.getElementById('value_results_STP').innerHTML
+    var seat_value_STC = document.getElementById('value_results_STC').innerHTML
+    var seat_value_FCA = document.getElementById('value_results_FCA').innerHTML
+    var seat_value_FCP = document.getElementById('value_results_FCP').innerHTML
+    var seat_value_FCC = document.getElementById('value_results_FCC').innerHTML
+   
+    price_STA = seat_price_array['STA'][sale_or_non_sale] * seat_value_STA
+    price_STP = seat_price_array['STP'][sale_or_non_sale] * seat_value_STP
+    price_STC = seat_price_array['STC'][sale_or_non_sale] * seat_value_STC
+    price_FCA = seat_price_array['FCA'][sale_or_non_sale] * seat_value_FCA
+    price_FCP = seat_price_array['FCP'][sale_or_non_sale] * seat_value_FCP
+    price_FCC = seat_price_array['FCC'][sale_or_non_sale] * seat_value_FCC
+   
+
+    total_price = (typeof (price_STA) != 'undefined' ? price_STA : 0) + (typeof (price_STP) != 'undefined' ? price_STP : 0) + (typeof (price_STC) != 'undefined' ? price_STC : 0) + (typeof (price_FCA) != 'undefined' ? price_FCA : 0) + (typeof (price_FCP) != 'undefined' ? price_FCP : 0) + (typeof (price_FCC) != 'undefined' ? price_FCC : 0);
+    document.getElementById('total').innerHTML = total_price
+}
+function checkDate() {
+    var today = new Date()
+    var today_year = today.getFullYear()
+    var today_month = today.getMonth() + 1
+    if (today_month < 10) {
+        min_expiry = today_year + "-0" + today_month 
+    }
+    else {
+        min_expiry = today_year + "-" + today_month
+    }
+    document.getElementById('cust_expiry').min = min_expiry
+}
