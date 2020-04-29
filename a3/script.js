9eac7e0224874ec09cdafe6e7edf6b1bb4dd28a5
s3751881 <tr.baonhi3008@gmail.com>
2020-04-29 22:11:50 +0700

    *add* hour and modify button

--- a3/script.js
+++ a3/script.js
@@ -25,16 +25,14 @@ let movies_syn = {
     'synopsisACT': {
         name: 'Avengers: Endgame',
         rating: 'M',
-        movieday: [ 'Wednesday: 9pm', 'Thursday: 9pm', 'Friday 9pm', 'Saturday: 6pm ', 'Sunday: 6pm'],
-        hour:['-', '-', '21:00', '21:00', '21:00', '18:00', '18:00'],
+        movieday: [ 'Wednesday : 21:00', 'Thursday : 21:00', 'Friday : 21:00', 'Saturday : 18:00 ', 'Sunday : 18:00'],
         plot: String.raw`After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...`,
         trailer: 'https://www.youtube.com/embed/TcMBFSGVi1c'
     },
     'synopsisAHF': {
         name: 'The Happy Prince',
         rating: 'MA15+',
-        movieday:['Wednesday','Thursday','Saturday','Sunday'],
-        hour: ['-', '-', '12:00', '12:00', '12:00', '21:00', '21:00'],
+        movieday:['Wednesday : 12:00','Thursday : 12:00','Friday : 12:00 ','Saturday : 21:00','Sunday : 21:00'],      
         plot: String.raw`In a cheap Parisian hotel room Oscar Wilde lies on his death bed. The past floods back, taking him to other times and places. Was he once the most famous man in London? The artist crucified by a society that once worshipped him? Under the microscope of death he reviews the failed attempt to reconcile with his long suffering wife Constance, the ensuing reprisal of his fatal love affair with Lord Alfred Douglas and the warmth and devotion of Robbie Ross, who tried and failed to save him from himself. Travelling through Wilde's final act and journeys through England, France and Italy, the transience of lust is laid bare and the true riches of love are revealed. It is a portrait of the dark side of a genius who lived and died for love.`,
         trailer: 'https://www.youtube.com/embed/4HmN9r1Fcr8'
     },
@@ -42,16 +40,14 @@ let movies_syn = {
     'synopsisRMC': {
         name: 'Top End Wedding',
         rating: 'M',
-        movieday:['Monday','Tuesday','Saturday','Sunday'],
-        hour: ['18:00', '18:00', '-', '-', '-', '15:00', '15:00'],
+        movieday:['Monday : 18:00','Tuesday : 18:00','Saturday : 15:00','Sunday : 15:00'],
         plot: String.raw`Lauren and Ned are engaged, they are in love, and they have just ten days to find Lauren's mother who has gone AWOL somewhere in the remote far north of Australia, reunite her parents and pull off their dream wedding.`,
         trailer: 'https://www.youtube.com/embed/uoDBvGF9pPU'
     },
     'synopsisANM': {
         name: 'Dumbo',
         rating: 'PG',
-        movieday:['Monday','Tuesday','Wednesday','Thursday','Saturday','Sunday'],
-        hour: ['12:00', '12:00', '18:00', '18:00', '18:00', '12:00', '12:00'],
+        movieday:['Monday : 12:00','Tuesday : 12:00','Wednesday : 18:00 ','Thursday : 18:00','Saturday : 12:00','Sunday : 12:00'],
         plot: String.raw`Holt was once a circus star, but he went off to war and when he returned it had terribly altered him. Circus owner Max Medici (Danny DeVito) hires him to take care of Dumbo, a newborn elephant whose oversized ears make him the laughing stock of the struggling circus troupe. But when Holt's children discover that Dumbo can fly, silver-tongued entrepreneur V.A. Vandevere (Michael Keaton), and aerial artist Colette Marchant (Eva Green) swoop in to make the little elephant a star.`,
         trailer: 'https://www.youtube.com/embed/7NiYVoqBt-8'
     },
@@ -76,70 +72,25 @@ function showContent(event){
     trailer.src = movie.trailer;
     movie_syn_input.value = event.target.id;
     synopsis_sec.style.display = '';
-    movie_showtime = movie.movieday + movie.hour
-    createDetailContainer(movie_showtime);
+    createDetailContainer(movie.movieday);
     console.log('clicked')
 }
 
 
-// const createTimeButton = (time, index) => {
-//     let button = document.createElement('Button');
-//     button.setAttribute('class', 'btn btn-secondary movie_hour');
-//     button.setAttribute('style', 'margin: 2% 2% 2% 0;');
-//     button.setAttribute('id', 'movie_hour_' + index);
-//     button.onclick = (e) => e.preventDefault();
-// //    create span
-//     let information= document.createElement('SPAN');
-//     information.innerText = time;
-//     information.setAttribute('class', 'day');
-//     button.appendChild(information)
-//     return button
-// }
-const createTimeButton = (hour,day,index) => {
+
+const createTimeButton = (time, index) => {
     let button = document.createElement('Button');
     button.setAttribute('class', 'btn btn-secondary movie_hour');
-    button.setAttribute('style', 'margin: 2% 2% 2% 0;');
+    button.setAttribute('style', 'margin: 2% 2% 2% 0;',"font-family: 'Oswald', sans-serif;");
     button.setAttribute('id', 'movie_hour_' + index);
     button.onclick = (e) => e.preventDefault();
-//    create span day 
-    let information_day= document.createElement('SPAN');
-    information_day.innerText = day;
-    information_day.setAttribute('class', 'day');
-    let information_hour = document.createElement('SPAN');
-    information_hour.innerText = hour;
-    information_hour.setAttribute('class','hour')
-    button.appendChild(information_day,information_hour)
-// hour   
+//    create span
+    let information= document.createElement('SPAN');
+    information.innerText = time;
+    information.setAttribute('class', 'day');
+    button.appendChild(information)
     return button
 }
-
-// const createTimeButton = (time, index) => {
-//     let button = document.createElement('Button');
-//     button.setAttribute('class', 'btn btn-secondary movie_hour');
-//     button.setAttribute('style', 'margin: 2% 2% 2% 0;');
-//     button.setAttribute('id', 'movie_hour_' + index);
-//     button.onclick = (e) => e.preventDefault();
-// //    create span
-//     let information = document.createElement('SPAN');
-//     information.innerText = time;
-//     information.setAttribute('class',  'day');
-//     button.appendChild(information)
-//     return button
-// }
-
-// const createDetailContainer = (time)=>{
-//     let index = 0;
-//     let detailContainer = document.getElementById('timetable');
-//     detailContainer.innerHTML = '';
-//     detailContainer.setAttribute('class', 'col-sm-9');
-//     detailContainer.setAttribute('style', 'color:black; display:block');
-//     detailContainer.setAttribute('id', 'timetable');
-//     for (let item of time) {
-//         const detailButton = createTimeButton(item, index);
-//         index++;
-//         detailContainer.append(detailButton);
-//     }
-// }
 const createDetailContainer = (time)=>{
     let index = 0;
     let detailContainer = document.getElementById('timetable');
@@ -147,16 +98,21 @@ const createDetailContainer = (time)=>{
     detailContainer.setAttribute('class', 'col-sm-9');
     detailContainer.setAttribute('style', 'color:black; display:block');
     detailContainer.setAttribute('id', 'timetable');
-    for (let item1,item2 of time) {
-        const detailButton = createTimeButton(item1,item2,index);
+    for (let item of time) {
+        const detailButton = createTimeButton(item, index);
         index++;
         detailContainer.append(detailButton);
     }
 }
+let buttons_time = document.querySelectorAll('#movie_hour_','.btn')
+function showContent_time(event){
+    var movie_time = movies_syn[event.target.id]
+    movie_dt = movie_time.movieday
+    console.log('clicked')
 
-
+} // cái hàm này em làm để khi click vào thì dữ liệu truyền vào hàm home ở dưới nè 
+buttons_time.forEach(button => {button.addEventListener('click',(event)=>showContent_time(event))})
 buttons.forEach(button => {button.addEventListener('click',(event)=>showContent(event))})
-
 
 function hour(obj) {
     x = obj.children[0].innerHTML
