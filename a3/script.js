6c840971b91fc2780d66d885ab8bf0b5c089c95d
s3751881 <tr.baonhi3008@gmail.com>
2020-05-01 00:18:35 +0700

    add css + modify js

--- a3/script.js
+++ a3/script.js
@@ -1,5 +1,5 @@
 /* Insert your javascript here */
-// Navigation Bar 
+// Navigation Bar
 let navbar_height = document.getElementsByTagName('nav')[0].clientHeight
 console.log(document.getElementsByTagName('nav'))
 const aboutus_sec = document.getElementById('aboutus').offsetTop - 30
@@ -25,14 +25,14 @@ let movies_syn = {
     'synopsisACT': {
         name: 'Avengers: Endgame',
         rating: 'M',
-        movieday: [ 'Wednesday : 21:00', 'Thursday : 21:00', 'Friday : 21:00', 'Saturday : 18:00 ', 'Sunday : 18:00'],
+        movieday: [ 'Wednesday: 21:00', 'Thursday: 21:00', 'Friday: 21:00', 'Saturday: 18:00 ', 'Sunday: 18:00'],
         plot: String.raw`After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...`,
         trailer: 'https://www.youtube.com/embed/TcMBFSGVi1c'
     },
     'synopsisAHF': {
         name: 'The Happy Prince',
         rating: 'MA15+',
-        movieday:['Wednesday : 12:00','Thursday : 12:00','Friday : 12:00 ','Saturday : 21:00','Sunday : 21:00'],      
+        movieday:['Wednesday: 12:00','Thursday: 12:00','Friday: 12:00 ','Saturday: 21:00','Sunday: 21:00'],      
         plot: String.raw`In a cheap Parisian hotel room Oscar Wilde lies on his death bed. The past floods back, taking him to other times and places. Was he once the most famous man in London? The artist crucified by a society that once worshipped him? Under the microscope of death he reviews the failed attempt to reconcile with his long suffering wife Constance, the ensuing reprisal of his fatal love affair with Lord Alfred Douglas and the warmth and devotion of Robbie Ross, who tried and failed to save him from himself. Travelling through Wilde's final act and journeys through England, France and Italy, the transience of lust is laid bare and the true riches of love are revealed. It is a portrait of the dark side of a genius who lived and died for love.`,
         trailer: 'https://www.youtube.com/embed/4HmN9r1Fcr8'
     },
@@ -40,14 +40,14 @@ let movies_syn = {
     'synopsisRMC': {
         name: 'Top End Wedding',
         rating: 'M',
-        movieday:['Monday : 18:00','Tuesday : 18:00','Saturday : 15:00','Sunday : 15:00'],
+        movieday:['Monday: 18:00','Tuesday: 18:00','Saturday: 15:00','Sunday: 15:00'],
         plot: String.raw`Lauren and Ned are engaged, they are in love, and they have just ten days to find Lauren's mother who has gone AWOL somewhere in the remote far north of Australia, reunite her parents and pull off their dream wedding.`,
         trailer: 'https://www.youtube.com/embed/uoDBvGF9pPU'
     },
     'synopsisANM': {
         name: 'Dumbo',
         rating: 'PG',
-        movieday:['Monday : 12:00','Tuesday : 12:00','Wednesday : 18:00 ','Thursday : 18:00','Saturday : 12:00','Sunday : 12:00'],
+        movieday:['Monday: 12:00','Tuesday: 12:00','Wednesday: 18:00 ','Thursday: 18:00','Saturday: 12:00','Sunday: 12:00'],
         plot: String.raw`Holt was once a circus star, but he went off to war and when he returned it had terribly altered him. Circus owner Max Medici (Danny DeVito) hires him to take care of Dumbo, a newborn elephant whose oversized ears make him the laughing stock of the struggling circus troupe. But when Holt's children discover that Dumbo can fly, silver-tongued entrepreneur V.A. Vandevere (Michael Keaton), and aerial artist Colette Marchant (Eva Green) swoop in to make the little elephant a star.`,
         trailer: 'https://www.youtube.com/embed/7NiYVoqBt-8'
     },
@@ -72,25 +72,29 @@ function showContent(event){
     trailer.src = movie.trailer;
     movie_syn_input.value = event.target.id;
     synopsis_sec.style.display = '';
+    // movie_showtime = movie.movieday + movie.hour
     createDetailContainer(movie.movieday);
     console.log('clicked')
+    var buttons_time = document.querySelectorAll('.movie_hour')
+    buttons_time.forEach(button=>{button.addEventListener('click', function(){addInputValue(this)})})
+    buttons_time.forEach(button=>{button.addEventListener('click', function(){hour(this)})})
+    buttons_time.forEach(button=>{button.addEventListener('click', function(){total()})})
 }
-
-
 
 const createTimeButton = (time, index) => {
     let button = document.createElement('Button');
     button.setAttribute('class', 'btn btn-secondary movie_hour');
-    button.setAttribute('style', 'margin: 2% 2% 2% 0;',"font-family: 'Oswald', sans-serif;");
+    button.setAttribute('style', 'margin: 2% 2% 2% 0;');
     button.setAttribute('id', 'movie_hour_' + index);
     button.onclick = (e) => e.preventDefault();
 //    create span
-    let information= document.createElement('SPAN');
+    let information= document.createElement('span');
     information.innerText = time;
     information.setAttribute('class', 'day');
     button.appendChild(information)
     return button
 }
+
 const createDetailContainer = (time)=>{
     let index = 0;
     let detailContainer = document.getElementById('timetable');
@@ -104,24 +108,23 @@ const createDetailContainer = (time)=>{
         detailContainer.append(detailButton);
     }
 }
-let buttons_time = document.querySelectorAll('#movie_hour_','.btn')
-function showContent_time(event){
-    var movie_time = movies_syn[event.target.id]
-    movie_dt = movie_time.movieday
-    console.log('clicked')
 
-} // cái hàm này em làm để khi click vào thì dữ liệu truyền vào hàm home ở dưới nè 
-buttons_time.forEach(button => {button.addEventListener('click',(event)=>showContent_time(event))})
 buttons.forEach(button => {button.addEventListener('click',(event)=>showContent(event))})
 
+
 function hour(obj) {
-    x = obj.children[0].innerHTML
-    y = obj.children[1].innerHTML
+    day_info = obj.children[0].innerHTML
+    day_value = day_info.substring(0 , day_info.indexOf(':'))
+    hour_value = day_info.substring(day_info.indexOf(':') + 2, day_info.lastIndexOf(':'))
+    console.log(day_value)
+    console.log(hour_value)
     var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    if (x == "Monday" || x == "Wednesday" || (Number(y) == 12 && weekdays.includes(x) == true)) {
+    if (day_value == "Monday" || day_value == "Wednesday" || (Number(hour_value) == 12 && weekdays.includes(day_value) == true)) {
+        console.log('Sale')
         return document.getElementById('results').innerHTML = 'Sale';
     }
     else {
+        console.log('NoSale')
         return document.getElementById('results').innerHTML = 'Non_sale';
     }
 }
@@ -176,8 +179,42 @@ function total() {
    
 
     total_price = (typeof (price_STA) != 'undefined' ? price_STA : 0) + (typeof (price_STP) != 'undefined' ? price_STP : 0) + (typeof (price_STC) != 'undefined' ? price_STC : 0) + (typeof (price_FCA) != 'undefined' ? price_FCA : 0) + (typeof (price_FCP) != 'undefined' ? price_FCP : 0) + (typeof (price_FCC) != 'undefined' ? price_FCC : 0);
-    document.getElementById('total').innerHTML = total_price
+    document.getElementById('total').innerHTML = total_price.toFixed(2)
 }
+
+function addInputValue(obj) {
+    day_info = obj.children[0].innerHTML
+    day_value = day_info.substring(0 , day_info.indexOf(':'))
+    hour_value = day_info.substring(day_info.indexOf(':') + 2, day_info.lastIndexOf(':'))
+    console.log(hour_value)
+    movie_day = document.getElementById('movie_day_input')
+    if (day_value == 'Monday') {
+        movie_day.value = 'MON';
+    }
+    if (day_value == 'Tuesday') {
+        movie_day.value = 'TUE';
+    }
+    if (day_value == 'Wednesday') {
+        movie_day.value = 'WED';
+    }
+    if (day_value == 'Thursday') {
+        movie_day.value = 'THU';
+    }
+    if (day_value == 'Friday') {
+        movie_day.value = 'FRI';
+    }
+    if (day_value == 'Saturday') {
+        movie_day.value = 'SAT';
+    }
+    if (day_value == 'Sunday') {
+        movie_day.value = 'SUN';
+    }
+    movie_hour = document.getElementById('movie_hour_input')
+    movie_hour.value = "T" + Number(hour_value)
+    console.log(movie_day)
+    console.log(movie_hour)
+}
+
 function checkDate() {
     var today = new Date()
     var today_year = today.getFullYear()
@@ -189,4 +226,4 @@ function checkDate() {
         min_expiry = today_year + "-" + today_month
     }
     document.getElementById('cust_expiry').min = min_expiry
-}
+    }
